816385d74387c54904a74f6a061c65a6bca95662
Manas <97386467+un-conditioned@users.noreply.github.com>
2022-01-19 15:29:23 +0530

    Update Faucet.sol

--- Faucet.sol
+++ Faucet.sol
@@ -1,9 +1,6 @@
-// SPDX-License-Identifier: CC-BY-SA-4.0
-
-// Version of Solidity compiler this program was written for
 pragma solidity 0.6.4;
 
-// Our first contract is a faucet!
+
 contract Faucet {
     // Accept any incoming amount
     receive() external payable {}
